1d167cce87c78bfa2b6ba298b9d3fcc15c1ab300
divyanshu rawat <divyanshu.r46956@gmail.com>
2016-08-22 23:23:13 +0530

    Added support for following a user.

--- follow.js
+++ follow.js
@@ -1,3 +1,5 @@
+
+//if you want to unfollow a user execute these set of instructions.
 var a = document.getElementsByClassName('Button TwoStateButton User pressed main_button user_follow_button');;
 
 function sleep (time) {
@@ -8,4 +10,18 @@ for (var idx= 0; idx < a.length; ++idx)
 {
     sleep();
      a[idx].click(3000);
+}
+
+
+// if you want to follow a user execute these set of instructions.
+var a = document.getElementsByClassName('Button TwoStateButton User main_button user_follow_button');
+
+function sleep (time) {
+  setTimeout(function(){ console.log("Hello"); }, time);
+}
+
+for (var idx= 0; idx < a.length; ++idx)
+{
+    sleep(3000);
+     a[idx].click();
 }
